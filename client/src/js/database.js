f3999acb062e90a4a645fb60bcdaf8640f6a77f9
Callum Mackintosh <callum.e.s.mackintosh@gmail.com>
2023-04-11 15:34:47 +1000

    whole lota work

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,27 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Method for adding to the database
+export const putDb = async (content) => {
+  console.log('PUT to the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  await store.add(content);
+  await tx.complete;
+  console.log('Content added to database:', content);
+}
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// Method for getting content
+export const getDb = async () => {
+  console.log('GET from the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  console.log('request.value', result);
+  return result;
+}
 
 initdb();
